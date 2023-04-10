ae23a52a944abc6d3804035be9c3537c0d5d68ee
code-tamer3000 <parfenov-art@mail.ru>
2023-04-10 15:09:23 +0500

    solution of task 2 1 was restructured

--- task_2_1.cpp
+++ task_2_1.cpp
@@ -10,41 +10,76 @@
 
 #include <iostream> 
 #include <vector> 
-#include<queue>
+#include <queue>
 
-std::vector<std::vector<int>> make_graph() {
-    int m = 0;
-    int n = 0;
+class ListGraph {
+ public:
+  explicit ListGraph(size_t vertices_count);
+ 
+  void AddEdge(size_t from, size_t to);
+ 
+  size_t VerticesCount() const;
+ 
+  std::vector<size_t> FindAllAdjacentIn(size_t vertex) const;
+  std::vector<size_t> FindAllAdjacentOut(size_t vertex) const;
+ 
+ private:
+  std::vector<std::vector<size_t>> in_edges_;
+  std::vector<std::vector<size_t>> out_edges_;
+};
+
+ListGraph::ListGraph(size_t vertices_count) : in_edges_(vertices_count), out_edges_(vertices_count) {
+}
+
+void ListGraph::AddEdge(size_t from, size_t to) {
+  in_edges_[to].push_back(from);
+  out_edges_[from].push_back(to);
+}
+ 
+size_t ListGraph::VerticesCount() const {
+  return in_edges_.size();
+}
+ 
+std::vector<size_t> ListGraph::FindAllAdjacentIn(size_t vertex) const {
+  return in_edges_[vertex];
+}
+std::vector<size_t> ListGraph::FindAllAdjacentOut(size_t vertex) const {
+  return out_edges_[vertex];
+}
+
+ListGraph make_graph() {
+    size_t m = 0;
+    size_t n = 0;
     std::cin >> n >> m;
-    std::vector<std::vector<int>> graph(n);
-    for (int i = 0; i < m; i++) {
-        int v1 = 0;
-        int v2 = 0;
+    ListGraph graph(n);
+    for (size_t i = 0; i < m; i++) {
+        size_t v1 = 0;
+        size_t v2 = 0;
         std::cin >> v1 >> v2;
-        graph[v1].push_back(v2);
-        graph[v2].push_back(v1);
+        graph.AddEdge(v1, v2);
+        graph.AddEdge(v2, v1);
     }
     return graph;
 }
 
-int BFS(std::vector<std::vector<int>>& graph, int start) {
-  std::vector<char> color(graph.size(), 0);
-  std::vector<std::pair<int, int>> path(graph.size(), {0, 0});
-  std::queue<int> to_go;
-  int st =-1;
-  int end = -1;
+size_t BFS(const ListGraph& graph, size_t start) {
+  std::vector<char> color(graph.VerticesCount());
+  std::vector<std::pair<size_t, size_t>> path(graph.VerticesCount());
+  std::queue<size_t > to_go;
+  size_t st =-1;
+  size_t end = -1;
   to_go.push(start);
   to_go.push(-1);
   color[start] = 1;
-  int level = 1;
+  size_t  level = 1;
   while (!to_go.empty() && st == -1) {
-    int u = to_go.front(); to_go.pop();
+    size_t  u = to_go.front(); to_go.pop();
     if (u == -1) {
       if (!to_go.empty()) to_go.push(-1);
       level++;
       continue;
     }
-    for (int v : graph[u]) {
+    for (size_t v : graph.FindAllAdjacentOut(u)) {
       if (color[v] == 0) {
         to_go.push(v);
         color[v] = 1;
@@ -58,23 +93,23 @@ int BFS(std::vector<std::vector<int>>& graph, int start) {
   }
   if (st == -1) return -1;
   else {
-    int cycle_lenght = 0;
+    size_t cycle_length = 0;
     if (path[st].second != path[end].second) {
-      cycle_lenght += 2;
+      cycle_length += 2;
       st = path[st].first;
-    } else cycle_lenght++;
+    } else cycle_length++;
     while(st != end) {
-      cycle_lenght += 2;
+      cycle_length += 2;
       st = path[st].first;
       end = path[end].first;
     }
-    return cycle_lenght;
+    return cycle_length;
   }
 }
 
-int find_min_cycle(std::vector<std::vector<int>>& graph) {
+int find_min_cycle(const ListGraph& graph) {
   int ans = -1;
-  for (int i = 0; i < graph.size(); i++) {
+  for (size_t i = 0; i < graph.VerticesCount(); i++) {
     int cycle = BFS(graph, i);
     if (cycle != -1) (ans != -1) ? ans = std::min(ans, cycle) : ans = cycle;
   }
@@ -82,7 +117,7 @@ int find_min_cycle(std::vector<std::vector<int>>& graph) {
 }
 
 int main() {
-    std::vector<std::vector<int>> graph = make_graph();
+    ListGraph graph = make_graph();
     std::cout << find_min_cycle(graph) << '\n';
     return 0;
 }
