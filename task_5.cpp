90b5ac214560927509c9f9bfddc32cef1c893673
code-tamer3000 <parfenov-art@mail.ru>
2023-04-16 22:16:12 +0500

    ListGraph was used

--- task_5.cpp
+++ task_5.cpp
@@ -19,17 +19,59 @@ USD/RUB: 0.02
 #include <vector>
 #include <float.h>
 
-double bellman_ford(std::vector<std::vector<double>>& matrix, int start) {
-    std::vector<double> distances(matrix.size(), DBL_MIN); 
+struct WeightedVertex {
+    double weight = 0; 
+    int vertex = 0;
+    WeightedVertex(double weight_, int from_or_to_) : weight(weight_) , vertex(from_or_to_) {}
+};
+
+class ListGraph {
+ public:
+    explicit ListGraph(int vertices_count);
+ 
+    void AddEdge(int from, int to, double weight);
+ 
+    int VerticesCount() const;
+ 
+    std::vector<WeightedVertex> FindAllAdjacentIn(int vertex) const;
+    std::vector<WeightedVertex> FindAllAdjacentOut(int vertex) const;
+ 
+ private:
+    std::vector<std::vector<WeightedVertex>> in_edges_;
+    std::vector<std::vector<WeightedVertex>> out_edges_;
+};
+
+ListGraph::ListGraph(int vertices_count) : in_edges_(vertices_count), out_edges_(vertices_count) {
+}
+
+void ListGraph::AddEdge(int from, int to, double weight) {
+    in_edges_[to].push_back(WeightedVertex(weight, from));
+    out_edges_[from].push_back(WeightedVertex(weight, to));
+}
+ 
+int ListGraph::VerticesCount() const {
+    return in_edges_.size();
+}
+ 
+std::vector<WeightedVertex> ListGraph::FindAllAdjacentIn(int vertex) const {
+    return in_edges_[vertex];
+}
+std::vector<WeightedVertex> ListGraph::FindAllAdjacentOut(int vertex) const {
+    return out_edges_[vertex];
+}
+
+
+double bellman_ford(ListGraph& graph, int start) {
+    std::vector<double> distances(graph.VerticesCount(), DBL_MIN); 
     distances[start] = 1; 
     bool not_processed = true;
-    for (int i = 0; i < matrix.size() - 1 && not_processed; i++) {
+    for (int i = 0; i < graph.VerticesCount() - 1 && not_processed; i++) {
         not_processed = false;
-        for (int u = 0; u < matrix.size(); u++) {
-            for (int v = 0; v < matrix.size(); v++) {
-                if (matrix[u][v] > 0) { 
-                    if (distances[u] != DBL_MIN && distances[v] < distances[u] * matrix[u][v]) {
-                        distances[v] = distances[u] * matrix[u][v];
+        for (int u = 0; u < graph.VerticesCount(); u++) {
+            for (auto v : graph.FindAllAdjacentOut(u)) {
+                if (v.weight > 0) { 
+                    if (distances[u] != DBL_MIN && distances[v.vertex] < distances[u] * v.weight) {
+                        distances[v.vertex] = distances[u] * v.weight;
                         not_processed = true;
                     }
                 }
@@ -39,9 +81,9 @@ double bellman_ford(std::vector<std::vector<double>>& matrix, int start) {
     return distances[start]; 
 }
 
-bool arbitrage(std::vector<std::vector<double>>& matrix) {
-    for (int i = 0; i < matrix.size(); i++) {
-        double tmp = bellman_ford(matrix, i);
+bool arbitrage(ListGraph& graph) {
+    for (int i = 0; i < graph.VerticesCount(); i++) {
+        double tmp = bellman_ford(graph, i);
         if (tmp > 1) return true;
     }
     return false;
@@ -50,13 +92,17 @@ bool arbitrage(std::vector<std::vector<double>>& matrix) {
 int main() {
     int n = 0;
     std::cin >> n;
-    std::vector<std::vector<double>> matrix(n, std::vector<double>(n, 0.));
+    ListGraph graph(n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == j) std::cin.ignore();
-            else std::cin >> matrix[i][j];
+            else {
+                double tmp = 0;
+                std::cin >> tmp;
+                graph.AddEdge(i, j, tmp);
+            }
         }
     }    
-    std::cout << ((arbitrage(matrix)) ? "YES" : "NO") << '\n';
+    std::cout << ((arbitrage(graph)) ? "YES" : "NO") << '\n';
     return 0;
 }
